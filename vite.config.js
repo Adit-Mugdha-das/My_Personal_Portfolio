59cd258fc1b559906f5a9f6e3a0854e6c8e29df6
Adit-Mugdha-Das <mailbox.mugdha@gmail.com>
2025-04-06 20:05:13 +0600

    Add Vite build output for Render

--- vite.config.js
+++ vite.config.js
@@ -8,4 +8,8 @@ export default defineConfig({
       refresh: true,
     }),
   ],
+  build: {
+    outDir: 'public/build', // 👈 this ensures manifest.json lands in the right place
+    emptyOutDir: true,
+  },
 });
