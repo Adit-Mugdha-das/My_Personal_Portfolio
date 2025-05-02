02392b735a3bc041c9b09cfa5d114ce1987ae803
Adit-Mugdha-Das <mailbox.mugdha@gmail.com>
2025-05-02 16:54:54 +0600

    project hamm

--- resources/views/projects.blade.php
+++ resources/views/projects.blade.php
@@ -4,6 +4,8 @@
   <meta charset="UTF-8" />
   <title>Projects - Mugdha</title>
   @vite(['resources/css/app.css', 'resources/js/app.js'])
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css" />
+
 
  <!-- <link rel="stylesheet" href="{{ secure_asset('build/assets/app-Ct2FSlyM.css') }}">
 <script src="{{ secure_asset('build/assets/app-eMHK6VFw.js') }}" defer></script>-->
